0fb81286b515e02bd1de7fc0a9583fd9ae21bc59
pravintp <pravin@testpress.in>
2021-12-07 17:11:21 +0530

    Create dashboard

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -26,3 +27,8 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "registration/login.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
