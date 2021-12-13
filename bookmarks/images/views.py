7d799d552db9f574e8d48175ed1a27322938dc4e
pravintp <pravin@testpress.in>
2021-12-13 18:37:26 +0530

    Add support to post images from other websites

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import ImageCreateForm
 
 # Create your views here.
+
+
+@login_required
+def image_create(request):
+    if request.method == "POST":
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = request.user
+            image.save()
+            messages.success(request, "Image added successfully")
+            return redirect(image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request, "images/image/create.html", {"form": form})
