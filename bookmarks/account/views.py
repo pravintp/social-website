43eb8fc06ba7468347c37dcbb675d6ef25b31d5e
pravintp <pravin@testpress.in>
2021-12-13 16:33:54 +0530

    Display notifications using message framework

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .utils import authenticate_user, add_user
@@ -51,6 +52,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
 
         return render(
             request,
