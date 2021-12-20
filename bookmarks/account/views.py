ea92b061fd03476a8e29b16de7a9549036f686f8
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-20 19:41:14 +0530

    Create login and logout view to the site (#1)

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import login
+
+from .forms import LoginForm
+from .utils import authenticate_user
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate_user(request, form)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+                return HttpResponse("Disabled account")
+            return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+    return render(request, "registration/login.html", {"form": form})
