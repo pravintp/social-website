a1aee88b6e8c551a3e9d8099199fdbfa1aa58d22
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-27 14:22:38 +0530

    Add user registration view (#6)

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
+    path("register/", views.register, name="register"),
 ]
