44415107d806175f1b0a4bc093fea6cf909884b1
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-28 10:50:22 +0530

    Create and use ajax decorator for the liking function (#19)

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -31,6 +32,7 @@ def image_detail(request, id, slug):
     )
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like_view(request):
